31ea1bc5baa89145978a218ed92cd3750e60f263
Comagol <comabellafrancisco@gmail.com>
2025-08-01 19:31:37 -0300

    add the events to control disconnected, error and sigint on the conection with the databse

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,8 +4,10 @@ import dotenv from 'dotenv';
 //Cargar variables de entorno
 dotenv.config();
 
+//URL de conexión a MongoDB
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/votacion';
 
+//Funcion para conectar a MongoDB 
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
@@ -16,3 +18,18 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
+
+// Manejar eventos de conexion
+mongoose.connection.on('disconnected', () => {
+  console.log('⚠️ Desconectado de MongoDB');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log('❌ Error de conexión a MongoDB', error);
+});
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  console.log('🔴 MongoDB desconectado');
+  process.exit(0);
+});
