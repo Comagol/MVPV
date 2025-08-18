dd1a80a343cd05bb98c64e20f2166c9fb00759ad
Comagol <comabellafrancisco@gmail.com>
2025-08-17 22:24:46 -0300

    Add user creation and login methods in UserService with email validation and token generation

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -20,4 +20,42 @@ export class UserService {
     };
   }
 
+  //Metodo para crear un usuario
+  async createUser(userData: CreateUserRequest): Promise<UserResponse> {
+    //verifico que el email no este registrado
+    const emailExist = await this.userDao.emailExists(userData.email);
+    if(emailExist) {
+      throw new Error('El email ya se encuentra registrado');
+    }
+    //creo el nuevo usuario
+    const newUser = await this.userDao.createUser(userData);
+    return this.formatUserResponse(newUser);
+  }
+
+  //Metodo para iniciar sesion(Login)
+  async login(credentials: LoginRequest): Promise<LoginResponse> {
+    //verificar las credenciales
+    const user = await this.userDao.verifyCredentials(credentials.email, credentials.password)
+    if(!user) {
+      throw new Error('Credenciales invalidas');
+    }
+    //verificio que el usuario este activo
+    if(!user.activo) {
+      throw new Error('El usuario esta inactivo');
+    }
+    //genero un token
+    const token = this.generateToken(user._id.toString());
+
+    return {
+      token,
+      user: {
+        id: user._id.toString(),
+        email: user.email,
+        nombre: user.nombre,
+        votosRealizados: user.votosRealizados,
+        activo: user.activo
+      }
+    };
+  }
+
 }
