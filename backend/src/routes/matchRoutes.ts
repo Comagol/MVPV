3a6f5471aaf3076ca6d5977cbde2ea5499512997
Comagol <comabellafrancisco@gmail.com>
2025-09-03 09:27:08 -0300

    Add route to create a new match with authentication and error handling

--- backend/src/routes/matchRoutes.ts
+++ backend/src/routes/matchRoutes.ts
@@ -31,11 +31,23 @@ router.get('/:id', async (req, res) => {
 });
 
 //ruta para obtener partidos activos
-router.get('/active', async (req, res) => {
+router.get('/active/matches', async (req, res) => {
   try {
     const activeMatches = await matchService.getActiveMatches();
     res.json(activeMatches);
   }catch (error: any) {
     res.status(500).json({ error: error.message});
+  }
+});
+
+//RUTAS PROTEGIDAS (solo admin)
+//Ruta para crear un nuevo partido
+router.post('/', authenticateToken, async (req, res) => {
+  try {
+    const matchData = req.body;
+    const match = await matchService.createMatch(matchData);
+    res.status(201).json({message: 'Partido creado correctamente', match});
+  }catch (error: any) {
+    res.status(400).json({ error: error.message });
   }
 });
