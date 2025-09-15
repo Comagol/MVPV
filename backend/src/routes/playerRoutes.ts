28fb40dc5a14a5f130ea491cd5dc59b17641a1fe
Comagol <comabellafrancisco@gmail.com>
2025-09-14 23:36:55 -0300

    Add route to create a new player with authentication and admin checks

--- backend/src/routes/playerRoutes.ts
+++ backend/src/routes/playerRoutes.ts
@@ -6,6 +6,18 @@ const router = express.Router();
 const playerService = new PlayerService();
 
 //RUTAS PRIVADAS
+
+// Ruta para crear un nuevo jugador
+router.post('/', authenticateToken, isAdmin, async (req, res) => {
+  try {
+    const playerData = req.body;
+    const player = await playerService.createPlayer(playerData);
+    res.status(201).json(player);
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+})
+
 //Ruta para obtener todos los jugadores
 router.get('/', authenticateToken, isAdmin, async (req, res) => {
   try {
